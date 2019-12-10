7d97b20d249503fcf31881263004c9b57f14bb68
muhammad-usman147 <ushakeel808@gmail.com>
2019-12-10 23:01:10 +0500

    4 basic apis

--- BOOTCAMP/TASK_1/RestingApi.py
+++ BOOTCAMP/TASK_1/RestingApi.py
@@ -5,20 +5,28 @@ app = Flask(__name__)
 firebase = firebase.FirebaseApplication('https://ninjabootcamp-b7626.firebaseio.com/',None)
 
 
-@app.route('/')
-def Main():
+@app.route('/PutData/<title>/<desc>')
+def Main(title,desc):
     
     data ={
-        'Title':'Machine Learning Enginner',
-        'Description':"Currently Working as in NASA"
+        'Title':str(title),
+        'Description':str(desc)
     }
-
-
-    result = firebase.get('ninjabootcamp-b7626/BootCampTable1','')
-    print("--------------------")
-    print(result['-LvfcXHBVbNiclUCMXQl']['Description'])
-    print("--------------------")
-    print(result['-LvfcXHBVbNiclUCMXQl']['Title'])
+    get_result = firebase.post('ninjabootcamp-b7626/',data)
+    
+    return jsonify(get_result)
+@app.route("/getData")
+def getdata():
+    result = firebase.get('ninjabootcamp-b7626/','')
     return jsonify(result)
+@app.route("/UpdateData/<id>/<title>/<desc>")
+def PutData(id,title,desc):
+    result1 = firebase.put('ninjabootcamp-b7626/{}'.format(str(id)),'Title',title)
+    result2=  firebase.put('ninjabootcamp-b7626/{}'.format(str(id)),'Description',desc)
+    return str(bool(result2))
+@app.route("/DeleteData/<id>")
+def DeleteData(id):
+    result1 = firebase.delete('ninjabootcamp-b7626/',id)
+    return jsonify(result1)
 if __name__ == "__main__":
     app.run(debug=True)
